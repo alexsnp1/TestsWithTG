f5e4adc469224012967f9514e448e12b7d0412b1
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-07-19 23:55:50 +0700

    added project

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+    public static final String SELENOID_URL = System.getProperty("selenoid_url");
+
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+    Configuration.remote = "https://user1:1234@" + SELENOID_URL + "/wd/hub";
+    Configuration.browser = System.getProperty("browser", "chrome");
+    Configuration.browserVersion = System.getProperty("browser.version", "128.0");
+    Configuration.browserSize = System.getProperty("browser_size", "1600x900");
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+            "enableVNC", true,
+            "enableVideo", true
+    ));
+    Configuration.browserCapabilities = capabilities;
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();;
+        Attach.addVideo();
+    }
+}
